4277a3ea193dffdf4774c79f737803157f873eb4
rifahmrg-tech <rifahmrg@gmail.com>
2025-08-25 17:48:33 +0530

    added some themes

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API=axios.create({
-    baseURL:"https://rifah-backend.onrender.com/",
+   baseURL:"https://rifah-backend.onrender.com/",
      //baseURL:"http://localhost:5000",
      withCredentials:true
 });
