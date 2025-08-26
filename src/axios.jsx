1695036670fae269190ca5baebff62da4df5b9d0
rifahmrg-tech <rifahmrg@gmail.com>
2025-08-26 14:42:18 +0530

    added backend and one small change

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API=axios.create({
-   //baseURL:"https://rifah-backend.onrender.com/",
-     baseURL:"http://localhost:5000",
+     baseURL:"https://rifah-backend.onrender.com/",
+     //baseURL:"http://localhost:5000",
      withCredentials:true
 });
 
